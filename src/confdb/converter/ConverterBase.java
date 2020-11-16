f2807b8959a1803d50d05d5385fa000d85ed9cd8
Sam James Harper <sharper@pcral6.dyndns.cern.ch>
2020-11-16 03:04:52 +0100

    fixes to parse tasks

--- src/confdb/converter/ConverterBase.java
+++ src/confdb/converter/ConverterBase.java
@@ -21,7 +21,8 @@ public class ConverterBase
 	
 	
     public ConverterBase( String format, Connection connection ) throws ConverterException
-    {
+    {   
+        System.out.println("correct confdb!!!!");
     	database = new ConfDB();
     	try {
     		database.connect( connection );
@@ -32,13 +33,15 @@ public class ConverterBase
     }
 	
     public ConverterBase( String format, String dbType, String dbUrl, String dbUser, String dbPwrd ) throws ConverterException
-    {
+    {       
     	this( format );
+	System.out.println("correct confdb!!!!");
     	initDB( dbType, dbUrl, dbUser, dbPwrd );
     }
   
     protected ConverterBase( String format ) throws ConverterException
-    {
+    {     
+	System.out.println("correct confdb!!!!");
 		try {
 			converterEngine = ConverterFactory.getConverterEngine( format );
 		} catch (Exception e) {
@@ -74,6 +77,7 @@ public class ConverterBase
 
     public IConfiguration getConfiguration( int key ) throws ConverterException
     {
+	System.out.println("config key "+key);
     	try {
 			return ConfCache.getCache().getConfiguration( key, getDatabase() );
 		} catch (DatabaseException e) {
