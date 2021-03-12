b9ec8bb48ab35b295b84067c23cf2f83f5d87055
Sam Harper <sam.james.harper@cern.ch>
2021-03-12 12:34:48 +0100

    fixing switch producer display

--- src/confdb/converter/python/PythonSwitchProducerWriter.java
+++ src/confdb/converter/python/PythonSwitchProducerWriter.java
@@ -4,18 +4,49 @@ import java.util.Iterator;
 
 import confdb.converter.ConverterEngine;
 import confdb.converter.ISwitchProducerWriter;
+import confdb.converter.ConverterException;
 import confdb.data.Reference;
+import confdb.data.Referencable;
+import confdb.data.ModuleInstance;
+import confdb.data.EDAliasInstance;
 import confdb.data.SwitchProducer;
 
 public class PythonSwitchProducerWriter implements ISwitchProducerWriter {
 	public String toString(SwitchProducer switchProducer, ConverterEngine converterEngine, String object) {
-		String str = object + switchProducer.name() + " = cms.SwitchProducer( ";
+	        String str = object + switchProducer.name() + " = CUDASwitchProducer(\n ";
+		String indent = "   ";
+	    
 		if (switchProducer.entryCount() > 0) {
-			String sep = " + ";
+
 			Iterator<Reference> list = switchProducer.entryIterator();
-			str += list.next().getPythonCode(object);
-			while (list.hasNext())
-				str += sep + list.next().getPythonCode(object);
+			
+			while (list.hasNext()){
+			    Reference ref = list.next();
+			    Referencable refObj = ref.parent();
+			    System.out.println("trying refObj "+refObj.name());
+			    String modStr = new String();
+			    if(refObj instanceof ModuleInstance){
+			
+				try {
+				    modStr=converterEngine.getModuleWriter().toString((ModuleInstance)refObj,indent);
+				} catch (ConverterException e) {
+				    System.out.println("error for refObj "+refObj.name()+" "+e.getMessage());
+				    modStr=e.getMessage();
+				}
+			    }else if(refObj instanceof EDAliasInstance){
+				try {
+				    modStr=converterEngine.getEDAliasWriter().toString((EDAliasInstance)refObj,indent);
+				} catch (ConverterException e) {
+				    System.out.println("error for refObj "+refObj.name()+" "+e.getMessage());
+				    modStr=e.getMessage();
+				}
+			    }	
+			    modStr = modStr.replaceFirst(switchProducer.name()+"_","");
+			    modStr = modStr.trim();
+			    modStr += ",\n";
+			    str += "  "+modStr;
+			}
+					       
 		}
 		str += " )\n";
 		return str;
