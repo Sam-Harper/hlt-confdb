0a9aa99f61ea5a674cfdb134f1ba77703ea6a0c7
Sam Harper <sam.james.harper@cern.ch>
2021-03-12 12:34:48 +0100

    fixing switch producer display

--- src/confdb/converter/IEDAliasWriter.java
+++ src/confdb/converter/IEDAliasWriter.java
@@ -5,5 +5,6 @@ import confdb.data.EDAliasInstance;
 public interface IEDAliasWriter extends ConverterEngineSetter {
 
 	public String toString(EDAliasInstance edAlias) throws ConverterException;
+        public String toString(EDAliasInstance edAlias, String indent) throws ConverterException;
 
 }
--- src/confdb/converter/IModuleWriter.java
+++ src/confdb/converter/IModuleWriter.java
@@ -5,5 +5,6 @@ import confdb.data.ModuleInstance;
 public interface IModuleWriter extends ConverterEngineSetter {
 	
 	public String toString( ModuleInstance module ) throws ConverterException;
+        public String toString( ModuleInstance module, String indent ) throws ConverterException;
 
 }
