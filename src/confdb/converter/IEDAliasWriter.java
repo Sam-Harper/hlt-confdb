8d26efacb4121f03a5293a9ce3928b5976ebbe59
Sam Harper <sam.j.harper@gmail.com>
2021-03-12 17:23:27 +0100

    making indent methods default

--- src/confdb/converter/IEDAliasWriter.java
+++ src/confdb/converter/IEDAliasWriter.java
@@ -5,6 +5,8 @@ import confdb.data.EDAliasInstance;
 public interface IEDAliasWriter extends ConverterEngineSetter {
 
 	public String toString(EDAliasInstance edAlias) throws ConverterException;
-        public String toString(EDAliasInstance edAlias, String indent) throws ConverterException;
+        default public String toString(EDAliasInstance edAlias, String indent) throws ConverterException {
+	    return toString(edAlias);
+	}
 
 }
